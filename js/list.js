cf7f98dedd6a8f2e2a59a536088bd625bff7ce76
Amol <amolshrivastava448@gmail.com>
2020-10-22 20:35:31 +0530

    name of js files changed.

---
+++ js/list.js
@@ -0,0 +1,17 @@
+const gridCont = document.querySelector('.grid-container');
+const sidebar = document.querySelector('.sidebar-sidebar');
+const main = document.querySelector('.main-content');
+const form = document.querySelector('.search-form');
+
+// toggle btn
+const toggleBtn = document.querySelector('.togglebtn');
+
+togglebtn.addEventListener('click', () => {
+  console.log('toggle btn clicked');
+  sidebar.classList.toggle('show');
+  togglebtn.classList.toggle('highlighted');
+})
+
+form.addEventListener('submit',e => {
+  e.preventDefault();
+})
