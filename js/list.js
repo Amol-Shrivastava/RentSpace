528c6d1edfe98d012ca91302deed9f15a0bc5a8b
Amol <amolshrivastava448@gmail.com>
2020-10-24 01:33:29 +0530

    code redundancy to create list element eliminated

--- js/list.js
+++ js/list.js
@@ -20,7 +20,7 @@ const removeBtn = document.querySelector('.remove-btn');
 
 //ul list for options -> city, room-config, rate
 const cityUl = document.getElementById('option_list_item--city');
-const roomUl = document.getElementById('option_list_item--room');
+const roomUl = document.getElementById('option_list_item--room-config');
 const rateUl = document.getElementById('option_list_item--rate');
 
 // toggle btn
@@ -33,117 +33,107 @@ togglebtn.addEventListener('click', () => {
 })
 
 //form submit
-form.addEventListener('submit',e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
 })
 
-
-//selection functionality
-
-sidebar.addEventListener('click', e =>{
-  // console.log(e.target);
-  if(e.target.classList.contains('option')){
-
-    //creating a list item corresponding to the clicked item
-    let selected_filter_items = document.createElement('li');
-    selected_filter_items.className = 'filter_items selected_filter_item';
-    selected_filter_items.innerHTML = `
+// creating list element for when option is selected to add to selected container
+let liElementSelected = (value, cont) => {
+  let selected_filter_items = document.createElement('li');
+  selected_filter_items.className = 'filter_items selected_filter_item';
+  selected_filter_items.innerHTML = `
     <label class="filter__name selected-filter" id="selected_filter_items">
-      <input type="checkbox" checked>
-      <span class="option">${e.target.innerHTML}</span>
-      </label>
+    <input type="checkbox" checked>
+    <span class="option">${value}</span>
+    </label>
 
-      <div class="remove-btn">
-        &times;
-      </div>
-      `;
+    <div class="remove-btn">
+      &times;
+    </div>
+    `;
 
-      // Adding newly created list item in the selected container
-      selectedUl.appendChild(selected_filter_items);
+  cont.appendChild(selected_filter_items);
+}
 
-      // Removing selected list from its container
-      let parentList = e.target.parentNode.parentNode;
-      parentList.remove();
+// creating list element for when option is deselected from selected container
+let liElementsUnselected = (value, cont) => {
+  let unselectedListItem = document.createElement('li');
+  unselectedListItem.className = 'filter_items';
+  unselectedListItem.innerHTML = `
+  <label class="filter__name" id= "${value}">
+    <input type="checkbox" for= "${value}" class="input-box">
+        <span class="option">${value}</span>
+    </label>
+    `;
+  console.log(unselectedListItem);
+  cont.appendChild(unselectedListItem);
+}
+
+
+//selection functionality****************************************
+sidebar.addEventListener('click', e => {
+
+  //1. for when name of option is clicked*******************************************
+  if (e.target.classList.contains('option')) {
+    let valuefilled = e.target.innerHTML;
+    liElementSelected(valuefilled, selectedUl);
+
+    // Removing selected list from its container
+    let parentList = e.target.parentNode.parentNode;
+    parentList.remove();
+  }
+
+
+  //2. for when name of input is clicked*******************************************
+  if (e.target.classList.contains('input-box')) {
+    let nextOption = e.target.nextElementSibling.innerText.toUpperCase();
+    liElementSelected(nextOption, selectedUl);
+    // Removing selected list from its container
+    let parentList = e.target.parentNode.parentNode;
+    parentList.remove();
+  }
+
+  //3. for when cross button is clicked******************************
+  if (e.target.classList.contains('remove-btn')) {
+
+    let selectedparentList, selectedFilterItem, selectedoptionName, selectedoptionValue;
+
+    selectedparentList = e.target.parentNode;
+    selectedFilterItem = e.target.previousElementSibling;
+    selectedoptionName = selectedparentList.querySelector('.option');
+    selectedoptionValue = selectedoptionName.innerText.toUpperCase();
+
+
+    // for city list readdition ***************************
+    if (selectedoptionValue === 'BHOPAL' ||
+      selectedoptionValue === 'INDORE' ||
+      selectedoptionValue === 'JABALPUR' ||
+      selectedoptionValue === 'SAGAR') {
+      selectedparentList.remove();
+
+      liElementsUnselected(selectedoptionValue, cityUl);
+
     }
 
-    //input box function
-    if (e.target.classList.contains('input-box')){
-      let nextOption = e.target.nextElementSibling.innerText.toUpperCase();
-      // console.log(nextOption);
+    // for room list readdition ***************************
+    if (selectedoptionValue === '1 BHK' ||
+      selectedoptionValue === '2 BHK' ||
+      selectedoptionValue === '1 ROOM' ||
+      selectedoptionValue === '2 ROOMS') {
 
-      let selected_filter_items = document.createElement('li');
-      selected_filter_items.className = 'filter_items selected_filter_item';
-      selected_filter_items.innerHTML = `
-      <label class="filter__name selected-filter" id="selected_filter_items">
-        <input type="checkbox" checked>
-        <span class="option">${nextOption}</span>
-        </label>
-
-        <div class="remove-btn">
-          &times;
-        </div>
-        `;
-        // Adding newly created list item in the selected container
-        selectedUl.appendChild(selected_filter_items);
-
-        // Removing selected list from its container
-        let parentList = e.target.parentNode.parentNode;
-        parentList.remove();
+      selectedparentList.remove();
+      liElementsUnselected(selectedoptionValue, roomUl);
     }
 
-    //remove option functionality
-    if(e.target.classList.contains('remove-btn')){
-      // let unselectedListItemCity, unselectedListItemRoom, unselectedListItemRate;
-      let selectedparentList, selectedFilterItem, selectedoptionName, selectedoptionValue;
+    // for rate list readdition ***************************
+    if (selectedoptionValue === '6000RS+' ||
+      selectedoptionValue === '5000RS-6000RS' ||
+      selectedoptionValue === '4000RS-5000RS' ||
+      selectedoptionValue === '2000RS-4000RS' ||
+      selectedoptionValue === '1000RS-2000RS') {
+      selectedparentList.remove();
 
-        selectedparentList = e.target.parentNode;
-        selectedFilterItem = e.target.previousElementSibling;
-        selectedoptionName = selectedparentList.querySelector('.option');
-        selectedoptionValue = selectedoptionName.innerText.toUpperCase();
-
-      // console.log(selectedoptionValue);
-
-      if(selectedoptionValue === 'BHOPAL' || selectedoptionValue === 'INDORE' || selectedoptionValue === 'JABALPUR' || selectedoptionValue === 'SAGAR'){
-        selectedparentList.remove();
-
-      let unselectedListItemCity = document.createElement('li');
-        unselectedListItemCity.className = 'filter_items';
-        unselectedListItemCity.innerHTML = `
-        <label id="${selectedoptionValue}" class="filter__name">
-          <input type="checkbox" for="${selectedoptionValue}">
-          <span class="option">${selectedoptionValue}</span>
-        </label>
-        `;
-        cityUl.appendChild(unselectedListItemCity);
-      }
-
-       if(selectedoptionValue === '1 BHK' || selectedoptionValue === '2 BHK' || selectedoptionValue === '1 ROOM' || selectedoptionValue === '2 ROOMS'){
-        selectedparentList.remove();
-
-        let unselectedListItemRoom = document.createElement('li');
-        unselectedListItemRoom.className = 'filter_items';
-        unselectedListItemRoom.innerHTML = `
-        <label id="${selectedoptionValue}" class="filter__name">
-          <input type="checkbox" for="${selectedoptionValue}">
-          <span class="option">${selectedoptionValue}</span>
-        </label>
-        `;
-        // console.log(unselectedListItemRoom);
-        roomUl.appendChild(unselectedListItemRoom);
-      }
-
-       if(selectedoptionValue === '6000RS+' || selectedoptionValue === '5000RS-6000RS' || selectedoptionValue === '4000RS-5000RS' || selectedoptionValue === '2000RS-4000RS' || selectedoptionValue === '1000RS-2000RS'){
-        selectedparentList.remove();
-
-      let unselectedListItemRate = document.createElement('li');
-      unselectedListItemRate.className = 'filter_items';
-      unselectedListItemRate.innerHTML = `
-      <label id="${selectedoptionValue}" class="filter__name">
-        <input type="checkbox" for="${selectedoptionValue}">
-        <span class="option">${selectedoptionValue}</span>
-      </label>
-      `;
-      rateUl.appendChild(unselectedListItemRate);
+      liElementsUnselected(selectedoptionValue, rateUl);
     }
   }
 })
