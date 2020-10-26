5d6a5772d2b26ad86c5637292cd07b0d5451ef7c
Amol <amolshrivastava448@gmail.com>
2020-10-26 21:01:31 +0530

    Image preview functionality corrected with del btn

--- js/lessor_form.js
+++ js/lessor_form.js
@@ -2,6 +2,8 @@ const houseImgInput = document.getElementById('houseImg');
 const houseImg = document.getElementById('mainhouseImg');
 const previewText = document.querySelector('.preview-img');
 const housePreviewCont = document.querySelector('.house-preview');
+const delBtn = Array.from(document.querySelectorAll('.del-btn'));
+
 
 const roomImgInput = document.getElementById('roomImg');
 const preview_img_text = Array.from(document.querySelectorAll('.preview-img--room'));
@@ -10,6 +12,7 @@ const room_two = document.getElementById('room-img-2');
 const room_three = document.getElementById('room-img-3');
 const room_four = document.getElementById('room-img-4');
 const room_five = document.getElementById('room-img-5');
+const roomBtn = document.getElementById('roomBtn');
 let i = 0;
 
 
@@ -18,11 +21,13 @@ houseImgInput.addEventListener('change', () => {
   let src = URL.createObjectURL(fileAdded);
   previewText.style.display = 'none';
   houseImg.style.display = 'block';
+  delBtn[0].classList.add('show');
   houseImg.src = src;
 })
 
 roomImgInput.addEventListener('change', () => {
   i++;
+
   let imgfiles = roomImgInput.files[0];
   // console.log(imgfiles);
 
@@ -34,30 +39,49 @@ roomImgInput.addEventListener('change', () => {
       case 1:
       preview_img_text[0].style.display = 'none';
       room_one.src = reader.result;
+      delBtn[1].classList.add('show');
       break;
 
       case 2:
       preview_img_text[1].style.display = 'none';
       room_two.src = reader.result;
+      delBtn[2].classList.add('show');
       break;
 
       case 3:
-        preview_img_text[2].style.display = 'none';
+      preview_img_text[2].style.display = 'none';
       room_three.src = reader.result;
+      delBtn[3].classList.add('show');
       break;
 
       case 4:
-        preview_img_text[3].style.display = 'none';
+      preview_img_text[3].style.display = 'none';
       room_four.src = reader.result;
+      delBtn[4].classList.add('show');
       break;
 
       case 5:
       preview_img_text[4].style.display = 'none';
       room_five.src = reader.result;
+      delBtn[5].classList.add('show');
+
+      roomBtn.style.display='none';
       break;
     }
   })
 
 reader.readAsDataURL(imgfiles);
 
+})
+
+delBtn.forEach(btn => {
+  btn.addEventListener('click', (e) =>{
+    // btn.classList.add('show');
+    console.log(e.target);
+    let previousImg = btn.previousElementSibling;
+       previousImg.src = "";
+       previousImg.style.display = 'none';
+       btn.nextElementSibling.style.display = 'flex';
+       btn.classList.remove('show');
+  })
 })
